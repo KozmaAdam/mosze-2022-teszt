16533b28001597927b2863f18bc316ef513329f9
Ádám Kozma <adam.kozma@hbpogroup.com>
2022-09-15 14:14:40 +0200

    Errors commented out

--- main.cpp
+++ main.cpp
@@ -4,22 +4,31 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
+	//typo N_ELEMENTS
     int *b = new int[NELEMENTS];
+	//no ; at the end of the line
     std::cout << '1-100 ertekek duplazasa'
+	//bad loop construction
     for (int i = 0;)
     {
         b[i] = i * 2;
     }
+	//bad loop construction
     for (int i = 0; i; i++)
     {
+		//no value to display, no ; at the end of the line
         std::cout << "Ertek:"
-    }    
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
+	//var only declared, no value
     int atlag;
+	//no ; at the end of the loop condition
     for (int i = 0; i < N_ELEMENTS, i++)
     {
+		//no ;
         atlag += b[i]
     }
+	// there's 
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
